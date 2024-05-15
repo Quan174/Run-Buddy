365fd120516716a2b0f2155fcff35db5d9c74df7
Kuant <58131272+Clakulis@users.noreply.github.com>
2024-05-15 23:36:54 +0700

    create route draft

--- app/src/main/java/com/example/group2_bigproject/CreateRoutePageActivity.java
+++ app/src/main/java/com/example/group2_bigproject/CreateRoutePageActivity.java
@@ -10,26 +10,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class CreateRoutePageActivity extends AppCompatActivity {
-    ImageView resultPageHomeButton;
-    TextView resultPageCreateRouteButton;
+    TextView createRoutePageBackButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.result_page);
-        resultPageHomeButton = findViewById(R.id.resultPageHomeButton);
-        resultPageCreateRouteButton = findViewById(R.id.resultPageCreateRouteButton;
-        resultPageHomeButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.create_route_page);
+
+        createRoutePageBackButton = findViewById(R.id.createRoutePageBackButton);
+        createRoutePageBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-//        resultPageCreateRouteButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(ResultPageActivity.this, CreateRoutePageActivity.class);
-//                startActivity(intent);
-//            }
-//        });
     }
 }
