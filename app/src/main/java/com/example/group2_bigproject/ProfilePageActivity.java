adc4fd59a9453a4999df110ad43e951b37664561
TTrungNg <88432947+TTrungNg@users.noreply.github.com>
2024-05-18 21:35:42 +0700

    profile page draft

--- app/src/main/java/com/example/group2_bigproject/ProfilePageActivity.java
+++ app/src/main/java/com/example/group2_bigproject/ProfilePageActivity.java
@@ -1,20 +1,18 @@
 package com.example.group2_bigproject;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class ProfilePageActivity extends AppCompatActivity implements DataListener{
-    FirebaseHelper fbHelper;
+public class ProfilePageActivity extends AppCompatActivity {
 
     ConstraintLayout mapSuggestedRoutesButton;
     ConstraintLayout mapSavedRoutesButton;
@@ -29,101 +27,140 @@ public class ProfilePageActivity extends AppCompatActivity implements DataListen
     TextView menuBarSocialButton;
     TextView menuBarProfileButton;
     TextView profilePageEditInformationButton;
-    private String userID;
-    private String name;
-    private String gender;
-    private String birthDay;
-    private String phone;
-    private String address;
-    private String height;
-    private String weight;
-    private TextView nameProfile;
-    private TextView genderProfile;
-    private TextView birthdayProfile;
-    private TextView phoneProfile;
-    private TextView addressProfile;
-    private TextView heightProfile;
-    private TextView weightProfile;
+    ConstraintLayout profilePagePersonalInformationButton;
+    ConstraintLayout profilePageCreatedRoutesButton;
+    ConstraintLayout profilePageActivityHistoryButton;
+    TextView profilePagePersonalInformationButtonText;
+    TextView profilePageCreatedRoutesButtonText;
+    TextView  profilePageActivityHistoryButtonText;
+    ConstraintLayout profilePagePersonalInformationLayout;
+    LinearLayout profilePageActivityHistoryLayout;
 
-    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_page);
-        fbHelper = new FirebaseHelper(this);
         menuBarHomeButton =findViewById(R.id.menuBarHomeButton);
         menuBarRoutesButton = findViewById(R.id.menuBarRoutesButton);
         menuBarMapButton = findViewById(R.id.menuBarMapButton);
         menuBarSocialButton = findViewById(R.id.menuBarSocialButton);
         menuBarProfileButton = findViewById(R.id.menuBarProfileButton);
         profilePageEditInformationButton = findViewById(R.id.profilePageEditInformationButton);
-        nameProfile = findViewById(R.id.textView46);
-        genderProfile = findViewById(R.id.profilePageGenderField);
-        birthdayProfile = findViewById(R.id.textView59);
-        phoneProfile = findViewById(R.id.textView60);
-        addressProfile = findViewById(R.id.textView61);
-        heightProfile = findViewById(R.id.textView62);
-        weightProfile = findViewById(R.id.textView63);
+        profilePagePersonalInformationButton = findViewById(R.id.profilePagePersonalInformationButton);
+        profilePageCreatedRoutesButton = findViewById(R.id.profilePageCreatedRoutesButton);
+        profilePageActivityHistoryButton = findViewById(R.id.profilePageActivityHistoryButton);
+        profilePagePersonalInformationButtonText = findViewById(R.id.profilePagePersonalInformationButtonText);
+        profilePageCreatedRoutesButtonText = findViewById(R.id.profilePageCreatedRoutesButtonText);
+        profilePageActivityHistoryButtonText = findViewById(R.id.profilePageActivityHistoryButtonText);
+        profilePagePersonalInformationLayout =  findViewById(R.id.profilePagePersonalInformationLayout);
+        profilePageActivityHistoryLayout = findViewById(R.id.profilePageActivityHistoryLayout);
 
-        userID = getIntent().getStringExtra("userID");
-        Toast.makeText(this, userID, Toast.LENGTH_SHORT).show();
-
-        fbHelper.readUser(userID, user -> {
-            nameProfile.setText(user.name);
-            genderProfile.setText(user.Gender);
-            birthdayProfile.setText(user.Birthday);
-            phoneProfile.setText(user.phone);
-            addressProfile.setText(user.address);
-            heightProfile.setText(String.format("%s cm", user.height));
-            weightProfile.setText(String.format("%s kg", user.weight));
-        });
+        Bundle bundle = getIntent().getExtras();
+        String userID = bundle.getString("userID", "Default");
 
         menuBarProfileButton.setTextColor(R.color.light_grey);
 
-        menuBarHomeButton.setOnClickListener(v -> {
-            Intent intent = new Intent(ProfilePageActivity.this, HomePageActivity.class);
-            intent.putExtra("userID", userID);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+        menuBarHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePageActivity.this, HomePageActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("userID", userID);
+                intent.putExtras(bundle);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
         });
 
-        menuBarRoutesButton.setOnClickListener(v -> {
-            Intent intent = new Intent(ProfilePageActivity.this, RoutesPage.class);
-            intent.putExtra("userID", userID);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+        menuBarRoutesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePageActivity.this, RoutesPage.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("userID", userID);
+                intent.putExtras(bundle);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
         });
 
-        menuBarMapButton.setOnClickListener(v -> {
-            Intent intent = new Intent(ProfilePageActivity.this, MapPageActivity.class);
-            intent.putExtra("userID", userID);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+        menuBarMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePageActivity.this, MapPageActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("userID", userID);
+                intent.putExtras(bundle);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
         });
 
-        menuBarSocialButton.setOnClickListener(v -> {
-            Intent intent = new Intent(ProfilePageActivity.this, SocialPageActivity.class);
-            intent.putExtra("userID", userID);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+        menuBarSocialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePageActivity.this, SocialPageActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("userID", userID);
+                intent.putExtras(bundle);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
         });
 
-        profilePageEditInformationButton.setOnClickListener(v -> {
-            ProfileEditFragment profileEditDiaglog = new ProfileEditFragment();
-            Bundle args = new Bundle();
-            args.putString("userID", userID);
-            profileEditDiaglog.setArguments(args);
-            profileEditDiaglog.show(getSupportFragmentManager(), "profileEdit");
+        profilePageEditInformationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProfileEditFragment profileEditDiaglog = new ProfileEditFragment();
+                profileEditDiaglog.show(getSupportFragmentManager(), "profileedit");
+            }
+        });
+
+        profilePagePersonalInformationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profilePagePersonalInformationButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                profilePagePersonalInformationButtonText.setTypeface(null, Typeface.BOLD);
+                profilePageCreatedRoutesButton.setBackgroundColor(Color.parseColor("#BBBBBB"));
+                profilePageCreatedRoutesButtonText.setTypeface(null, Typeface.BOLD);
+                profilePageActivityHistoryButton.setBackgroundColor(Color.parseColor("#BBBBBB"));
+                profilePageActivityHistoryButtonText.setTypeface(null, Typeface.BOLD);
+                profilePagePersonalInformationLayout.setVisibility(View.VISIBLE);
+                profilePageActivityHistoryLayout.setVisibility(View.GONE);
+            }
+        });
+
+        profilePageCreatedRoutesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profilePageCreatedRoutesButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                profilePageCreatedRoutesButtonText.setTypeface(null, Typeface.BOLD);
+                profilePagePersonalInformationButton.setBackgroundColor(Color.parseColor("#BBBBBB"));
+                profilePagePersonalInformationButtonText.setTypeface(null, Typeface.BOLD);
+                profilePageActivityHistoryButton.setBackgroundColor(Color.parseColor("#BBBBBB"));
+                profilePageActivityHistoryButtonText.setTypeface(null, Typeface.BOLD);
+                profilePagePersonalInformationLayout.setVisibility(View.GONE);
+                profilePageActivityHistoryLayout.setVisibility(View.GONE);
+            }
+        });
+
+        profilePageActivityHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profilePageActivityHistoryButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                profilePageActivityHistoryButtonText.setTypeface(null, Typeface.BOLD);
+                profilePagePersonalInformationButton.setBackgroundColor(Color.parseColor("#BBBBBB"));
+                profilePagePersonalInformationButtonText.setTypeface(null, Typeface.BOLD);
+                profilePageCreatedRoutesButton.setBackgroundColor(Color.parseColor("#BBBBBB"));
+                profilePageCreatedRoutesButtonText.setTypeface(null, Typeface.BOLD);
+                profilePagePersonalInformationLayout.setVisibility(View.GONE);
+                profilePageActivityHistoryLayout.setVisibility(View.VISIBLE);
+
+            }
         });
     }
-
-    @Override
-    public void onDataReceived(String data) {
-        userID = data;
-    }
-
 }
