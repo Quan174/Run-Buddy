b69a86eb2362bec355845cc5020e87421fe3dec9
DucNguyen1311 <duyduc.miaham@gmail.com>
2024-05-19 19:43:15 +0700

    Saving session to SharedPreferences. reformat code.

--- app/src/main/java/com/example/group2_bigproject/SignInPageActivity.java
+++ app/src/main/java/com/example/group2_bigproject/SignInPageActivity.java
@@ -30,46 +30,38 @@ public class SignInPageActivity extends AppCompatActivity {
         Intent signUpClick = new Intent(this, SignUpPageActivity.class);
         Button btn_SignIn = findViewById(R.id.btn_SignIn);
 
-        hereClickView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(signUpClick);
+        hereClickView.setOnClickListener(v -> startActivity(signUpClick));
+        btn_SignIn.setOnClickListener(v -> {
+            if (username.getText().toString().compareTo("DuckTheVit") == 0) {
+                userID = -69;
+                Intent intent = new Intent(SignInPageActivity.this, HomePageActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("userID", userID);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                Toast.makeText(SignInPageActivity.this,"Signed In",Toast.LENGTH_SHORT).show();
             }
-        });
-        btn_SignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (username.getText().toString().compareTo("DuckTheVit") == 0) {
-                    userID = -69;
-                    Intent intent = new Intent(SignInPageActivity.this, HomePageActivity.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putInt("userID", userID);
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-                    Toast.makeText(SignInPageActivity.this,"Signed In",Toast.LENGTH_SHORT).show();
-                }
-                if (username.getText().toString().isEmpty()) {
-                    Toast.makeText(SignInPageActivity.this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
-                }
-                if (password.getText().toString().isEmpty()) {
-                    Toast.makeText(SignInPageActivity.this, "Password cannot be empty!", Toast.LENGTH_SHORT).show();
-                } else if (!username.getText().toString().isEmpty()){
-                    int flags = helper.checkPassword(username.getText().toString(), password.getText().toString());
-                    switch (flags) {
-                        case 0:
-                            Intent intent = new Intent(SignInPageActivity.this, HomePageActivity.class);
-                            Bundle bundle = new Bundle();
-                            userID = helper.getUserIDFromUsername(username.getText().toString());;
-                            intent.putExtra("userID", userID);
-                            startActivity(intent);
-                            Toast.makeText(SignInPageActivity.this,"Signed In, userID is " + userID,Toast.LENGTH_SHORT).show();
-                            break;
-                        case 1:
-                            Toast.makeText(SignInPageActivity.this, "Wrong password!", Toast.LENGTH_SHORT).show();
-                            break;
-                        case 2:
-                            Toast.makeText(SignInPageActivity.this, "Username not registered!", Toast.LENGTH_SHORT).show();
-                    }
+            if (username.getText().toString().isEmpty()) {
+                Toast.makeText(SignInPageActivity.this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
+            }
+            if (password.getText().toString().isEmpty()) {
+                Toast.makeText(SignInPageActivity.this, "Password cannot be empty!", Toast.LENGTH_SHORT).show();
+            } else if (!username.getText().toString().isEmpty()){
+                int flags = helper.checkPassword(username.getText().toString(), password.getText().toString());
+                switch (flags) {
+                    case 0:
+                        Intent intent = new Intent(SignInPageActivity.this, HomePageActivity.class);
+                        Bundle bundle = new Bundle();
+                        userID = helper.getUserIDFromUsername(username.getText().toString());;
+                        intent.putExtra("userID", userID);
+                        startActivity(intent);
+                        Toast.makeText(SignInPageActivity.this,"Signed In, userID is " + userID,Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(SignInPageActivity.this, "Wrong password!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(SignInPageActivity.this, "Username not registered!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
