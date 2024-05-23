901c474e7358786c01cfdfdefb105db2e4266cd8
DucNguyen1311 <duyduc.miaham@gmail.com>
2024-05-23 13:49:48 +0700

    stuff

--- app/src/main/java/com/example/group2_bigproject/SearchPageActivity.java
+++ app/src/main/java/com/example/group2_bigproject/SearchPageActivity.java
@@ -1,6 +1,8 @@
 package com.example.group2_bigproject;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,7 @@ import java.util.ArrayList;
 
 public class SearchPageActivity extends AppCompatActivity {
     private FirebaseHelper fbHelper;
+    TextView peopleButton, routesButton;
 
     private SharedPreferencesHelper spHelper;
     private String userID;
@@ -24,8 +28,13 @@ public class SearchPageActivity extends AppCompatActivity {
     ImageView backButton;
     EditText toolBarSearchInput;
     ArrayList<User> listResults;
+    ArrayList<Route> listRoutes;
     SearchPageUserAdapter SearchPageUserAdapter;
+    RoutesPageRouteListViewAdapter routesPageRouteListViewAdapter;
+    Boolean isPeople = true;
 
+//    @SuppressLint("WrongViewCast")
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +44,29 @@ public class SearchPageActivity extends AppCompatActivity {
         fbHelper = new FirebaseHelper(this);
         listView = findViewById(R.id.listView);
         backButton = findViewById(R.id.backButton);
+        peopleButton = findViewById(R.id.peopleButton);
+        routesButton = findViewById(R.id.routesButton);
+
         toolBarSearchInput = findViewById(R.id.toolBarSearchInput);
         userID = spHelper.getSessionID();
 
         listResults = new ArrayList<>();
+        listRoutes = new ArrayList<>();
 
-
+        peopleButton.setOnClickListener(v -> {
+            if(!isPeople){
+                peopleButton.setBackground(getResources().getDrawable(R.drawable.sent_messages));
+                routesButton.setBackground(getResources().getDrawable(R.drawable.received_messages));
+                isPeople = true;
+            }
+        });
+        routesButton.setOnClickListener(v -> {
+            if(isPeople){
+                peopleButton.setBackground(getResources().getDrawable(R.drawable.received_messages));
+                routesButton.setBackground(getResources().getDrawable(R.drawable.sent_messages));
+                isPeople = false;
+            }
+        });
 
         backButton.setOnClickListener(v -> finish());
 
@@ -59,16 +85,25 @@ public class SearchPageActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start,
                                       int before, int count) {
                 if (s.length() != 0) {
-                    fbHelper.searchForUser(userID ,s.toString(), users -> {
-                        listResults = users;
-                        SearchPageUserAdapter = new SearchPageUserAdapter(listResults, SearchPageActivity.this);
-                        listView.setAdapter(SearchPageUserAdapter);
-                        listView.setOnItemClickListener((parent, view, position, id) -> {
+                    if(isPeople){
+                        fbHelper.searchForUser(userID ,s.toString(), users -> {
+                            listResults = users;
+                            SearchPageUserAdapter = new SearchPageUserAdapter(listResults, SearchPageActivity.this);
+                            listView.setAdapter(SearchPageUserAdapter);
+                            listView.setOnItemClickListener((parent, view, position, id) -> {
 
 //                            Intent intent = new Intent(SearchPageActivity.this, ViewProfilePageActivity.class);
 //                            startActivity(intent);
+                            });
                         });
-                    });
+                    } else {
+                        fbHelper.searchRouteByName(s.toString(), routes -> {
+                            listRoutes = routes;
+                            routesPageRouteListViewAdapter = new RoutesPageRouteListViewAdapter(listRoutes, SearchPageActivity.this);
+                            listView.setAdapter(routesPageRouteListViewAdapter);
+                        });
+                    }
+
                 }
             }
         });
